1d8d2125a4c3fbe38e98ec55d2e24fedea877496
Sere27 <vetbat1126@gmail.com>
2020-09-22 17:10:25 +0300

    global package added(simple-is)

--- global and local packages/paket1/index.js
+++ global and local packages/paket1/index.js
@@ -1,5 +1,6 @@
 console.log("paket1");
 
+// Node path i sistem değişkenlerine ekledik
+// npm install -g ile global kuruyoruz
 const is = require("simple-is");
-
-console.log(is.number(24));
+console.log("is.number(24) :>> ", is.number(24));
--- global and local packages/paket2/index.js
+++ global and local packages/paket2/index.js
@@ -1,1 +1,3 @@
 console.log("paket2");
+
+// paket 1 de ki simple is i burada kullanamayız.
