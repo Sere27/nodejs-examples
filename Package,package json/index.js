0160807503497a158c888d76b30f5f551db259cb
Sere27 <vetbat1126@gmail.com>
2020-09-22 17:38:06 +0300

    package json example imported

---
+++ Package,package json/index.js
@@ -0,0 +1,6 @@
+console.log("Bu benim uygulamam!");
+
+//node modules klasörümüz büyükse ve github yada bi server atmak istiyorsak
+//node modules silebiliriz. sonra npm install dersek package.jsonda dependencies e göre yükleme yapacak
+//development dependencies de bu packageları tekrar indirmemize gerek olmayacak production kısmına cıktıgımız zaman.
+//bir paketi development dependencies olarak eklemek istiyorsak -->> npm install webpack -cli --save-dev <<--
